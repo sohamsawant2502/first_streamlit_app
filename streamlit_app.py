73c8b8bb61a14458a026b746a9537eed2ccfec5f
sohamsawant2502 <88843478+sohamsawant2502@users.noreply.github.com>
2023-11-09 15:31:17 +0530

    Update streamlit_app.py
    added header 
    addded 3 line in text format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new Healthy Dinner')
+streamlit.header('My parents new Healthy Dinner')
+streamlit.text('Omega 3 Blueberry Oatmeal')
+streamlit.text('kale , Spinach & rocket smoothie')
+streamlit.text('hard-Boiled free-range egg')
